0d13a70fd52cbd7f25da207a929d8b2d4c71c196
Dani-203 <dani_33@standrews.ac.th>
2026-04-23 18:11:41 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+print(add(235445,3245645))
+print(subtract(3223,2123))
+print(multiply(44444,44445))
+print(divide(332332,3232))
+
+def gret (name):
+    print (name)
+name = input ("please enter your name")
+gret (name)
