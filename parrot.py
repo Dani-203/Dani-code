9738e4e16ac78accc3bd48ddbd023f9e3adf4754
Dani-203 <dani_33@standrews.ac.th>
2026-04-30 18:15:46 +0700

    Add files via upload

---
+++ parrot.py
@@ -0,0 +1,14 @@
+#class
+class parrot:
+
+        #variable
+        bird = "sparrow"
+
+        #constructor
+        def  __init__(self,name):
+                self.name = name
+
+#object
+p1 = parrot("Tommy")
+print(p1.name)
+print(p1.bird)
