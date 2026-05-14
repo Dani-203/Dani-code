177262257ae2ac7f41f3f721ca3e816559834514
Dani-203 <dani_33@standrews.ac.th>
2026-05-14 18:15:12 +0700

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+my_dict = {}
+my_dict = {1:'apple',2:"ball"}
+
+my_dict = {'name':'Jack','age':26}
+
+print(my_dict['name'])
+print(my_dict.get('age'))
+
+my_dict['address'] = 'Downtown'
+print(my_dict)
+
+my_dict.pop('age')
+print(my_dict)
