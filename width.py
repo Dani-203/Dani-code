a0f363d9674ed6cc16925d32c8e9a03d7b5e2157
Dani-203 <dani_33@standrews.ac.th>
2026-07-30 18:17:25 +0700

    Add files via upload

---
+++ width.py
@@ -0,0 +1,30 @@
+with open('Codingal.txt','w') as file:
+  file.write("Hi! I am Penguin and I am 1 yr old.")
+file.close()
+
+with open('Codingal.txt', 'r') as file:
+  data = file.readlines()
+  print("Words in this file are....")
+  for line in data:
+    word = line.split()
+    print (word)
+file.close()
+
+
+new_file = open('New_File.txt', 'x')
+new_file.close()
+
+
+import os
+print("Checking if my_file exists or not....")
+if os.path.exists("my_file.txt"):
+  os.remove("my_file.txt")
+else:
+  print("The file does not exists")
+my_file = open("my_file.txt","w")
+my_file.write("hi! I am Penguin and I am 1 yr old")
+my_file.close()
+
+os.remove('Codingal.txt')
+
+os.rmdir('Folder')
